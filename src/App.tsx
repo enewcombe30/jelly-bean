ba95cdedc010e7db2bd026f67e837ff5bf89d46a
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-09-13 08:25:19 +0100

    Title added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="w-full h-full">
-      <div className="w-fit m-auto">Base Typescript React App</div>
+      <div className="w-fit m-auto">Jelly Bean</div>
     </div>
   );
 }
